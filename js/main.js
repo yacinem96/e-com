5a686a8ec1ed74c9911c538c4d81bb53198bc49f
yacine_mahi <my4cin3m@gmail.com>
2022-01-08 23:11:47 +0100

    upload

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 var noms=["Banane","Fraise","Kiwi","Orange","Melon","Tomate","Pomme","Mangue","Viande"];
 var prices=[100,120,300,440,110,111,458,458,744];
 var images=[1,2,3,4,5,6,7,8,9];
-var paniers,produits=[];
-var htmlpanier,htmlproduit="";
+var paniers=[];
+var produits=[];
+var htmlpanier = "";
 
+var htmlproduit = "";
+document.getElementById("cpt").innerText=0;
 for (let i = 0; i < noms.length; i++) {
     const nom=noms[i];
     const prix=prices[i];
@@ -19,7 +22,7 @@ for (let i = 0; i < noms.length; i++) {
      produits.push(produit);
 
      htmlproduit+=`
-     <div class="card mt-3 ms-3" style="width: 18rem;">
+     <div class="card mt-3 ms-3 shadow" style="width: 18rem;">
      <img src="images/${image}.jpg" class="card-img-top mt-2" alt="..." width="100%"
      height="150px">
      <div class="card-body">
@@ -27,19 +30,38 @@ for (let i = 0; i < noms.length; i++) {
        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
        <span class="card-text" style="font-weight: bolder;"> ${prix}.00 DA</span>  
        
-       <a href="#" style='margin-left:55px' class="btn btn-success" id="ajoutp"><i class="fas fa-cart-plus"></i></a>
+       <a href="#" style='margin-left:57px' class="btn btn-success" onclick="ajoutP(${i})"><i class="fas fa-cart-plus"></i></a>
        <a href="#" class="btn btn-outline-danger btn-rounded"><i class="fas fa-heart"></i></a>
      </div>
    </div>`;
     
 }
 
+
 document.getElementById("produits").innerHTML=htmlproduit;
 
-var ajoutp=document.getElementById("ajoutp");
-ajoutp.onclick(e=>{
-  e.get
-});
+
+function ajoutP(i){
+  console.log("avant le push"+i);
+  paniers.push(produits[i]);
+ console.log("apres le push"+i);
+htmlpanier+=`<tr>
+<td>${i}</td>
+<td>${produits[i].nom}</td>
+<td>${produits[i].qte}</td>
+<td>${produits[i].prix}.00 DA</td>
+<td>
+<button class="btn btn-sm btn-success"><i class="fas fa-check"></i></button>
+<button class="btn btn-sm btn-danger"> <i class="fas fa-times"> </i></button>
+</td>
+</tr> `;
+document.getElementById("tableProduits").innerHTML=htmlpanier;
+document.getElementById("cpt").innerText=paniers.length;
+}
+
+
+
+
 
 
 
