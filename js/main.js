511e144f49fecbec6e45e9cbe6a6530c639ded0e
yacine_mahi <my4cin3m@gmail.com>
2022-01-20 15:17:07 +0100

    upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var noms=["Banane","Fraise","Kiwi","Orange","Melon","Tomate","Pomme","Mangue","Viande"];
 var prices=[100,120,300,440,110,111,458,458,744];
-var images=[1,2,3,4,5,6,7,8,9];
+var images=["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg"];
 var descriptions=["Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content.","Some quick example text to build on the card title and make up the bulk of the card's content."];
 var paniers=[];
 var produits=[];
@@ -8,6 +8,8 @@ var htmlpanier = "";
 
 var htmlproduit = "";
 document.getElementById("cpt").innerText=0;
+
+
 for (let i = 0; i < noms.length; i++) {
     const nom=noms[i];
     const prix=prices[i];
@@ -26,14 +28,14 @@ for (let i = 0; i < noms.length; i++) {
 
      htmlproduit+=`
      <div class="card mt-3 ms-3 shadow" style="width: 18rem;">
-     <img src="images/${image}.jpg" class="card-img-top mt-2" alt="..." width="100%"
-     height="150px">
+     <img src="images/${image}" class="card-img-top mt-2" alt="..." width="100%"
+     height="160px">
      <div class="card-body">
        <h5 class="card-title">${nom}</h5>
        <p class="card-text">${desc}</p>
        <span class="card-text" style="font-weight: bolder;"> ${prix}.00 DA</span>  
        
-       <a  style='margin-left:57px' class="btn btn-success" onclick="ajoutP(${i})"><i class="fas fa-cart-plus"></i></a>
+       <a  style='margin-left:56px' class="btn btn-success" onclick="ajoutP(${i})"><i class="fas fa-cart-plus"></i></a>
        <a  class="btn btn-outline-danger btn-rounded"><i class="fas fa-heart"></i></a>
      </div>
    </div>`;
@@ -105,29 +107,116 @@ htmlpanier="";
 tablePanier(i);
 }
 
-var btncree=document.getElementById("creep");
-btncree.onclick=function(){
-  console.log("btn click");
-  const nom=getElementById("inputNom").value;
-  const prix=getElementById("inputPrix").value;
-  const qte=getElementById("inputQte").value;
-  const image=getElementById("inputImg").value;
-  const desc=getElementById("txtDesc").value;
-  const id=produits.length
-  noms.push(nom);
 
-  console.log(noms);
+
+// creeProduit(e) {
+//   e.preventDefault();
+//   console.log("btn click");
+//   const nom=document.getElementById("inputNom").value;
+//   const prix=document.getElementById("inputPrix").value;
+//   const qte=document.getElementById("inputQte").value;
+//   const image=document.getElementById("inputImg").value;
+//   const desc=document.getElementById("txtDesc").value;
+//   const id=produits.length;
+//   var path =image.split(`fakepath\\`);
+//   const img =path[1];
+
+//   console.log(img);
+
+//   noms.push(nom);
+//   prices.push(prix);
+//   images.push(img);
+//   descriptions.push(desc);
+//   console.log(noms);
+
 //   produit={
 //     id:id,
 //     nom:nom,
 //     prix:prix,
-//     image:image,
+//     image:img,
 //     description:desc,
 //     qte:qte
 // };
 //   produits.push(produit);
+
+//   htmlproduit+=`
+//   <div class="card mt-3 ms-3 shadow" style="width: 18rem;">
+//   <img src="images/${img}" class="card-img-top mt-2" alt="..." width="100%"
+//   height="150px">
+//   <div class="card-body">
+//     <h5 class="card-title">${nom}</h5>
+//     <p class="card-text">${desc}</p>
+//     <span class="card-text" style="font-weight: bolder;"> ${prix}.00 DA</span>  
+    
+//     <a  style='margin-left:57px' class="btn btn-success" onclick="ajoutP(${id})"><i class="fas fa-cart-plus"></i></a>
+//     <a  class="btn btn-outline-danger btn-rounded"><i class="fas fa-heart"></i></a>
+//   </div>
+// </div>`;
+// }
+
+var btncree=document.getElementById("creep");
+btncree.onclick=function(e){
+  e.preventDefault();
+  console.log("btn click");
+  const nom=document.getElementById("inputNom").value;
+  const prix=document.getElementById("inputPrix").value;
+  const qte=document.getElementById("inputQte").value;
+  const image=document.getElementById("inputImg").value;
+  const desc=document.getElementById("txtDesc").value;
+  const id=produits.length;
+  var path =image.split(`fakepath\\`);
+  const img =path[1];
+
+  console.log(img);
+
+  noms.push(nom);
+  prices.push(prix);
+  images.push(img);
+  descriptions.push(desc);
+  console.log(noms);
+
+  produit={
+    id:id,
+    nom:nom,
+    prix:prix,
+    image:img,
+    description:desc,
+    qte:qte
+};
+  produits.push(produit);
+
+  htmlproduit+=`
+  <div class="card mt-3 ms-3 shadow" style="width: 18rem;">
+  <img src="images/${img}" class="card-img-top mt-2" alt="..." width="100%"
+  height="150px">
+  <div class="card-body">
+    <h5 class="card-title">${nom}</h5>
+    <p class="card-text">${desc}</p>
+    <span class="card-text" style="font-weight: bolder;"> ${prix}.00 DA</span>  
+    
+    <a  style='margin-left:50px' class="btn btn-success" onclick="ajoutP(${id})"><i class="fas fa-cart-plus"></i></a>
+    <a  class="btn btn-outline-danger btn-rounded"><i class="fas fa-heart"></i></a>
+  </div>
+</div>`;
+document.getElementById("produits").innerHTML=htmlproduit;
+
 }
 
+
+
+  
+  document.querySelector("#inputImg").addEventListener("change",function(){
+    console.log(this.files);
+
+    const reader =new FileReader();
+
+    reader.addEventListener("load" , ()=>{
+      localStorage.setItem("recent-image" , reader.result)
+    });
+
+    reader.readAsDataURL(this.files[0]);
+
+  });
 
 
 
